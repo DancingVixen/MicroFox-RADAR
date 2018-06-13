626cdc9b1c474312c27999e20a691524f15d67fc
RF-Fox <ibreakz2025@gmail.com>
2018-06-13 07:33:34 -0600

    LCD display works but not refreshing

--- MicroFox_RADAR.ino
+++ MicroFox_RADAR.ino
@@ -1,14 +1,20 @@
 /*
   uFOX 5.826Ghz Microwave RADAR 
  */
-
+//LCD Library
+#include <LiquidCrystal.h>
+//LCD Pin map
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 //pin assignment for input
-int Output = 4;
+int Output = 6;
 
 // Code run when Arduino Powered On/Reset
 void setup() {
   // Serial communications at 9600 baud rate
   Serial.begin(9600);
+  lcd.begin(16, 2);
+  lcd.print(Serial);
   //
   pinMode(Output, INPUT);
 }
@@ -19,5 +25,5 @@ void loop() {
   int SensorVal = digitalRead(Output);
   // print out the state of sensor:
   Serial.println(SensorVal);
-  delay(1);    // Delay between reading measurements for added stability.
+  delay(0.5);    // Delay between reading measurements for added stability.
 }
